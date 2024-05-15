8d4c8e8bb86489c65e66d782db207f4c64e732fe
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-05-15 18:53:48 -0400

    feat: add timeout payment status

--- Model/WebpayOrderData.php
+++ Model/WebpayOrderData.php
@@ -12,6 +12,7 @@ class WebpayOrderData extends AbstractModel implements \Magento\Framework\DataOb
     const PAYMENT_STATUS_FAILED = 'FAILED';
     const PAYMENT_STATUS_CANCELED_BY_USER = 'CANCELED_BY_USER';
     const PAYMENT_STATUS_ERROR = 'ERROR';
+    const PAYMENT_STATUS_TIMEOUT = 'TIMEOUT';
 
     /**
      * @return void
