0c9e013e218858152ece79d6f2e29b14c81b2270
Miguel Villalobos <miguel.villalobos@continuumhq.com>
2025-09-25 12:59:58 -0300

    feat: inscription query with QueryBuilder on oneclick
    Uses Magento's QueryBuilder to retrieve oneclick inscription data.
    
    This change improves code readability and maintainability by using
    Magento's database abstraction layer.

--- Helper/Inscriptions.php
+++ Helper/Inscriptions.php
@@ -1,12 +1,15 @@
 <?php
+
 namespace Transbank\Webpay\Helper;
+
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
 use Magento\Framework\App\ResourceConnection;
 use Transbank\Webpay\Model\ResourceModel\OneclickInscriptionData;
+use Magento\Framework\DB\Adapter\AdapterInterface;
 
-
-class Inscriptions extends AbstractHelper {
+class Inscriptions extends AbstractHelper
+{
     protected $oneclickInscriptionDataFactory;
     protected $resourceConnection;
     protected $session;
@@ -17,8 +20,8 @@ class Inscriptions extends AbstractHelper {
         \Magento\Customer\Model\Session $session,
         Context $context,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-        ResourceConnection $resourceConnection)
-    {
+        ResourceConnection $resourceConnection
+    ) {
         $this->oneclickInscriptionDataFactory = $oneclickInscriptionDataFactory;
         $this->session = $session;
         $this->resourceConnection = $resourceConnection;
@@ -30,18 +33,23 @@ class Inscriptions extends AbstractHelper {
         $customerId = $this->session->getCustomer()->getId();
 
         if (isset($customerId)) {
-            //Create Connection
+            /** @var AdapterInterface $connection */
             $connection = $this->resourceConnection->getConnection();
-            // get table name
-            $table = $connection->getTableName(OneclickInscriptionData::TABLE_NAME);
-            // Select query
-            $selectquery = "SELECT id, username, card_type, card_number FROM ".$table." WHERE user_id = ".$customerId." AND status = 'SUCCESS'";
-            $result = $connection->fetchAll($selectquery);
+            $tableName = $connection->getTableName(OneclickInscriptionData::TABLE_NAME);
+
+            $select = $connection->select()
+                ->from(
+                    ['t' => $tableName],
+                    ['id', 'username', 'card_type', 'card_number'] // Columns to select
+                )
+                ->where('t.user_id = ?', $customerId) // Placeholder for customerId
+                ->where('t.status = ?', 'SUCCESS'); // Placeholder for status
+
+            $result = $connection->fetchAll($select);
         } else {
             $result = [];
         }
 
         return $result;
-
     }
 }
