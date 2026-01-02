3f967d30be1c3c46841d8dac69e4fb3ab3b4054f
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2026-01-02 20:08:27 +0000

    chore: remove dockers

--- docker-magento-php7.4-mag2.4.4-apache/plugin_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-chsh -s /bin/bash www-data;
-php bin/magento module:disable Transbank_Webpay --clear-static-content;
-php bin/magento module:enable Transbank_Webpay --clear-static-content;
-php bin/magento setup:upgrade && php bin/magento setup:di:compile;
